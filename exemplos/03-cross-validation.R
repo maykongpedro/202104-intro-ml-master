e38b812b341d5aaefadb4426c505170d8cfc9b6b
Maykon G Pedro <maykonglaffite@gmail.com>
2021-06-15 15:45:58 -0300

    fechando aula 3

--- exemplos/03-cross-validation.R
+++ exemplos/03-cross-validation.R
@@ -13,9 +13,12 @@ diamonds_initial_split <- diamonds %>% initial_split(3/4)
 diamonds_train <- training(diamonds_initial_split)
 diamonds_test <- testing(diamonds_initial_split)
 
+
 # definicao do modelo -----------------------------------------------------
 # OBS: repare que agora colocamos "tune()" nos hiperparâmetros para os quais 
 # queremos encontrar o melhor valor.
+
+# aqui estamos tunando a complexidade, e não a altura da árvore
 diamonds_model <- decision_tree(
   cost_complexity = tune(),
   min_n = 2, 
@@ -24,30 +27,45 @@ diamonds_model <- decision_tree(
   set_engine("rpart") %>%
   set_mode("regression")
 
+
+
 # reamostragem com cross-validation ---------------------------------------
 diamonds_resamples <- vfold_cv(diamonds_train, v = 5)
 
+diamonds_resamples
+
 # tunagem de hiperparametros ----------------------------------------------
 diamonds_tune_grid <- tune_grid(
-  price ~ x, 
   diamonds_model,
+  price ~ x, 
   resamples = diamonds_resamples,
   grid = 10,
   metrics = metric_set(rmse, rsq),
-  control = control_grid(verbose = TRUE, allow_par = FALSE)
+  control = control_grid(verbose = TRUE, allow_par = FALSE) 
+  # com verbose igual a TRUE conseguimos saber onde o modelo está processando
 )
+
+diamonds_tune_grid
+
 
 # inspecao da tunagem -----------------------------------------------------
 autoplot(diamonds_tune_grid)
 collect_metrics(diamonds_tune_grid)
 show_best(diamonds_tune_grid)
 
+
 # seleciona o melhor conjunto de hiperparametros
 diamonds_best_hiperparams <- select_best(diamonds_tune_grid, "rmse")
+
 diamonds_model <- diamonds_model %>% finalize_model(diamonds_best_hiperparams)
-  
+
+
 # desempenho do modelo final ----------------------------------------------
-diamonds_last_fit <- diamonds_model %>% last_fit(price ~ x, split = diamonds_initial_split)
+diamonds_last_fit <- diamonds_model %>% last_fit(price ~ x, 
+                                                 split = diamonds_initial_split)
+
+diamonds_last_fit
+                                                 
 
 collect_metrics(diamonds_last_fit)
 collect_predictions(diamonds_last_fit) %>%
@@ -64,6 +82,7 @@ diamonds_com_previsao <- diamonds %>%
   mutate(
     price_pred = predict(diamonds_final_model, new_data = .)$.pred
   )
+
 
 # guardar o modelo para usar depois ---------------------------------------
 saveRDS(diamonds_final_model, file = "diamonds_final_model.rds")
